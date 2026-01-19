33352b1ccdfba42a0553e3da96fa65c512ecaa42
ruchiralakshan123 <ruchiralakshan2003@gmail.com>
2026-01-19 12:22:03 +0530

    initial header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header(){
+
+    return (
+        <div className="header">
+        <span className="header-text">Online Quiz</span>
+        </div>
+    )
+}
+
+export default Header
